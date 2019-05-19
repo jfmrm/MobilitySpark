6fb05cc2cbaea8319a32b09d8c5e1254916f27e2
mmps <mmps@cin.ufpe.br>
2019-05-19 18:12:40 +0000

    websocket server

--- websocket/server.py
+++ websocket/server.py
@@ -1,31 +1,41 @@
 import asyncio
 import websockets
 import pika
+import utm
 
-params = pika.ConnectionParameters('amqp://localhost',5672)
+params = pika.ConnectionParameters('127.0.0.1',5672)
 connection = pika.BlockingConnection(params)
 channel = connection.channel()
 
 def consume():
     payload = []
+    i=0
     for method_frame, properties, body in channel.consume('mobilityData'):
-        print(method_frame, properties, body)
+        #print(method_frame, properties, body)
         channel.basic_ack(method_frame.delivery_tag)
-        payload.append(body)
+        data = Dados(body.decode('utf-8'))
+        print(data)
+        payload.append(data)
+        i+=1
 
-        if method_frame.delivery_tag == 10:
+        if i == 10:
             break
     return payload
 
-def tratar():
-    pass
+def Dados(String):
+    Info = []
+    Stringlist = String.split(",")
+    Info.append(Stringlist[0])
+    Info.append(Stringlist[1])
+    GPS = utm.to_latlon(int(Stringlist[6]),int(Stringlist[7]), 25, "L")
+    Info.append(GPS[0])
+    Info.append(GPS[1])
+    return Info
 
-async def serve():
+async def serve(websocket, path):
     data = consume()
-    resp = tratar(data)
-    con = await websocket.recv()
 
-    await websocket.send(data)
+    await websocket.send(str(data))
 
 start_server = websockets.serve(serve, 'localhost', 8765)
 asyncio.get_event_loop().run_until_complete(start_server)
