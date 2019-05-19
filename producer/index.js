a44638a33f4246e3fe40b89e790f38d361929a08
João Filipe Moura <jfmrm@cin.ufpe.br>
2019-05-19 14:46:30 -0300

    Makes producer read from file and send each line to the rabbitMQ with 1s delay between each line

--- producer/index.js
+++ producer/index.js
@@ -1,20 +1,26 @@
 import amqp from './node_modules/amqplib/channel_api';
+import fs from 'fs';
+
+function throwData(data, i, channel) {
+    setTimeout(() => {
+        console.log(data[i])
+        channel.sendToQueue('mobilityData', new Buffer(data[i]));
+        throwData(data, i++, channel)
+    }, 1000)
+}
 
 export function startSending() {
     return amqp.connect('amqp://rabbitmq')
         .then((connection) => {
             return connection.createChannel()
         }).then((channel) => {
-            const queue = 'task_queue';
-            for(let i = 0; i < 10; i ++) {
-                let msg = `${i} function .....`;
-    
-                channel.assertExchange('logs1', 'fanout', {
-                    durable: false
-                });
-                channel.publish('logs1', '', Buffer.from(msg));
-                console.log(" [x] Sent '%s'", msg);
-            }
+            const q = 'mobilityData';
+            channel.assertQueue(q, { durable: false });
+
+            let data = fs.readFileSync("./data/200118_240118.csv", {encoding: 'utf8'})
+            data = data.split('\n')
+            console.log(data.length)
+            throwData(data, 1, channel)
         });
 }
 
