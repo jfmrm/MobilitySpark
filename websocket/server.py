8e9980632e37322ad0ecea1198b81b729b1a7531
mmps <mmps@cin.ufpe.br>
2019-05-20 17:57:34 +0000

    working map

--- websocket/server.py
+++ websocket/server.py
@@ -14,12 +14,11 @@ def consume():
         #print(method_frame, properties, body)
         channel.basic_ack(method_frame.delivery_tag)
         data = Dados(body.decode('utf-8'))
-        print(data)
         payload.append(data)
-        i+=1
-
+        i += 1
         if i == 10:
             break
+
     return payload
 
 def Dados(String):
@@ -28,14 +27,15 @@ def Dados(String):
     Info.append(Stringlist[0])
     Info.append(Stringlist[1])
     GPS = utm.to_latlon(int(Stringlist[6]),int(Stringlist[7]), 25, "L")
-    Info.append(GPS[0])
-    Info.append(GPS[1])
+    Info.append(str(GPS[0]))
+    Info.append(str(GPS[1]))
     return Info
 
 async def serve(websocket, path):
-    data = consume()
-
-    await websocket.send(str(data))
+    payload = consume()
+    for data in payload:
+        print(data)
+        await websocket.send(', '.join(data))
 
 start_server = websockets.serve(serve, 'localhost', 8765)
 asyncio.get_event_loop().run_until_complete(start_server)
